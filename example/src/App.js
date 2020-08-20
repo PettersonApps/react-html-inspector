e82042ed30a3fd3c1138058c9bb29b457f7548ca
Vitaliy Kovalenko <kovalenko.v92@gmail.com>
2020-08-20 17:52:03 +0300

    Update example

--- example/src/App.js
+++ example/src/App.js
@@ -1,10 +1,43 @@
-import React from 'react'
+import React, { useState } from 'react'
 
-import { ExampleComponent } from 'greenlight-inspector'
-import 'greenlight-inspector/dist/index.css'
+import HTMLInspector from 'react-html-inspector'
+
+const testAreaStyle = {
+	textAlign: 'center',
+	margin: 100
+}
 
 const App = () => {
-  return <ExampleComponent text="Create React Library Example 😄" />
+	const [active, setActive] = useState(false)
+
+	return (
+		<div id='root'>
+			<div id='test-area' style={testAreaStyle}>
+				<span>text text</span>
+				<br />
+				<br />
+				<button type='button'>THIS IS BUTTON</button>
+				<br />
+				<br />
+				<input type='text' />
+			</div>
+			<div style={{ textAlign: 'center' }}>
+				<input
+					id='checkbox'
+					type='checkbox'
+					checked={active}
+					onChange={() => setActive(!active)}
+					style={{ margin: 'auto' }}
+				/>
+				<label htmlFor='checkbox'>Toggle inspector</label>
+			</div>
+			<HTMLInspector
+				rootElementSelector='#test-area'
+				onElementClick={console.log}
+				active={active}
+			/>
+		</div>
+	)
 }
 
 export default App
