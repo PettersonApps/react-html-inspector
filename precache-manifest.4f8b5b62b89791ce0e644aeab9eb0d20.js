feee544be409acf1906f60d43ebed5872830f8fe
Vitaliy Kovalenko <vit.kovalenko@icloud.com>
2020-08-21 09:04:09 +0300

    Updates

---
+++ precache-manifest.4f8b5b62b89791ce0e644aeab9eb0d20.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ae3b1f4e22cabbabfe3fe01790d530c9",
+    "url": "./index.html"
+  },
+  {
+    "revision": "f5985901a8a18e37ba32",
+    "url": "./static/css/main.42cc8b22.chunk.css"
+  },
+  {
+    "revision": "56f1505b1dd792356ab6",
+    "url": "./static/js/2.acb856e6.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "./static/js/2.acb856e6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f5985901a8a18e37ba32",
+    "url": "./static/js/main.41173707.chunk.js"
+  },
+  {
+    "revision": "6fce53c7c7713ebf61712cc2929746fa",
+    "url": "./static/js/main.41173707.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "13608fd49694b500217f",
+    "url": "./static/js/runtime-main.280af060.js"
+  }
+]);
